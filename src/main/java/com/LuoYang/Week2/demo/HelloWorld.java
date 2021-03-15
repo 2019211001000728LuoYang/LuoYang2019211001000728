ae5cdca2ac5932ea6f67ad1916d3fe00a4f816ba
2019211001000728LuoYang <1729132215@qq.com>
2021-03-16 00:00:38 +0800

    HW-Week 2 Commit

--- src/main/java/com/LuoYang/Week2/demo/HelloWorld.java
+++ src/main/java/com/LuoYang/Week2/demo/HelloWorld.java
@@ -7,11 +7,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HelloWorldServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
-            throw IOException{
-            PrintWriter writer = response.getWriter();
-            writer.println("Hello Client !!!");
-        }
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello Client !!!");
     }
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    public void doPost(HttpServletRequest request, HttpServletResponse response){}
 }
